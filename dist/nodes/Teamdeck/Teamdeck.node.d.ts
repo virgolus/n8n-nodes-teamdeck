41dee04aef754e00857267c896f2ec454ba2ab0a
Ezio Vergine <ezio.vergine@neosperience.com>
2024-11-20 03:34:48 +0100

    delete build files

--- dist/nodes/Teamdeck/Teamdeck.node.d.ts
+++
@@ -1,5 +0,0 @@
-import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-export declare class Teamdeck implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
